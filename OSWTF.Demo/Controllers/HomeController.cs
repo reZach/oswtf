bd1f118a16d6f40fe3773b368addda0fec8ad9d0
Zachary Wenta <reZach@users.noreply.github.com>
2019-02-17 19:57:04 -0600

    Cleaning up the demo a little bit

--- OSWTF.Demo/Controllers/HomeController.cs
+++ OSWTF.Demo/Controllers/HomeController.cs
@@ -1,18 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using OSWTF.Demo.Models;
+using OSWTF.Server;
 
 namespace OSWTF.Demo.Controllers
 {
     public class HomeController : Controller
     {
+        [TypeFilter(typeof(ServerSideTestFilter),
+            Arguments = new object[] {new[] {"MyFirstTest"}})]
         public IActionResult Index()
         {
-            return View();
+            string variation = ViewBag.ServerSideTests["MyFirstTest"];
+            string result = string.Empty;
+
+            if (variation == "0")
+            {
+                result = "I fall in the control.";
+            }
+            else if (variation == "1")
+            {
+                result = "I fall in the variation.";
+            }
+
+            result +=
+                " Please delete the cookie and refresh this page to re-bucket this user into the control or variation";
+
+            return Content(result);
         }
 
         public IActionResult Privacy()
@@ -23,7 +37,7 @@ namespace OSWTF.Demo.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
--- OSWTF.Demo/Startup.cs
+++ OSWTF.Demo/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OSWTF.Server.Models;
 
 namespace OSWTF.Demo
 {
@@ -24,6 +25,8 @@ namespace OSWTF.Demo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<TestingOptions>(Configuration.GetSection("OSWTF"));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
